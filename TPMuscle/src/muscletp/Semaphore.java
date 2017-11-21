756fba66517d412984ca13f909be1ae7ef9391f2
ASUS <ASUS@ordi>
2017-11-21 11:16:58 +0100

    A voir...

--- TPMuscle/src/muscletp/Semaphore.java
+++ TPMuscle/src/muscletp/Semaphore.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public abstract class Semaphore {
+	
+	//A voir  peut etre de la généricité à ce niveau ou avec element de régulation...
 	Collection<Jonction> jonctionRegulees = new ArrayList<Jonction>();
 	private SegmentRoute segment;
 	private PositionSegment position;
